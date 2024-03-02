4caa3c89ff6f424ce9d896a4d3d5588a5938e040
Zilant <93409915+the-zilant@users.noreply.github.com>
2024-03-02 19:28:00 +0300

    Add files via upload

---
+++ agrosem/json/voc.js
@@ -0,0 +1,14 @@
+const request_voc = {
+	dev:{
+		'/html/farming.html':'Точне землеробство',
+		'/html/fertilizers.html':'Добрива',
+		'/html/index.html':'Головна',
+		'/html/splicing.html':'Зрошення'
+	},
+	prod:{
+		'farming':'Точне землеробство',
+		'fertilizers':'Добрива',
+		'/':'Головна',
+		'splicing':'Зрошення'
+	}
+};

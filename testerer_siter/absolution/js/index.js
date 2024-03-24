4091ccb001b12686f803b76f114d188713bddf55
Vsezlodov <vsezlodov@gmail.com>
2024-03-25 02:18:13 +0300

    Update index.js

--- testerer_siter/absolution/js/index.js
+++ testerer_siter/absolution/js/index.js
@@ -308,7 +308,7 @@ function scrollToHash(hash, e) {
   const elem = hash ? document.querySelector(hash) : false;
   if(elem) {
     if(e) e.preventDefault();
-    gsap.to(window, {scrollTo: elem});
+    gsap.to(window, {scrollTo: elem, duration: 2});
   }
 }
 

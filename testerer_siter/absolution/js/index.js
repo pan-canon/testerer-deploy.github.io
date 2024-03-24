a46d507b99b91f7c02c6c903a1392a132407d1ff
Vsezlodov <vsezlodov@gmail.com>
2024-03-24 08:41:13 +0300

    upo

--- testerer_siter/absolution/js/index.js
+++ testerer_siter/absolution/js/index.js
@@ -290,32 +290,7 @@ const scroll = () => {
 
 // Custom
 // Detect if a link's href goes to the current page
-function getSamePageAnchor (link) {
-  if (
-    link.protocol !== window.location.protocol ||
-    link.host !== window.location.host ||
-    link.pathname !== window.location.pathname ||
-    link.search !== window.location.search
-  ) {
-    return false;
-  }
 
-  return link.hash;
-}
-
-// Scroll to a given hash, preventing the event given if there is one
-function scrollToHash(hash, e) {
-  const elem = hash ? document.querySelector(hash) : false;
-  if(elem) {
-    if(e) e.preventDefault();
-    gsap.to(window, {scrollTo: elem});
-  }
-}
-
-
-
-// Scroll to the element in the URL's hash on load
-scrollToHash(window.location.hash);
 
 
 

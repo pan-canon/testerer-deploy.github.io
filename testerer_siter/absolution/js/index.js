2ca38bc73d952432b938ee9ea2b392348c68d67f
Vsezlotov <vsezlodov@gmail.com>
2024-03-30 16:57:59 +0300

    Update index.js

--- testerer_siter/absolution/js/index.js
+++ testerer_siter/absolution/js/index.js
@@ -42,7 +42,7 @@ const applyAnimation = (grid, animationType) => {
 			start: 'top bottom+=5%', 
 			end: 'bottom top-=5%', 
 			scrub: true, 
-			onEnter:selectBiopunk(event), 
+			onEnter: () => selectBiopunk(event), 
 		}
 	});
 

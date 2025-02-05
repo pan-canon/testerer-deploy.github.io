df67bacfdd6480fd5997d7e0c645f7f435db2b0a
Vsezlotov <vsezlodov@gmail.com>
2025-02-06 00:32:06 +0300

    uptd

--- Testerer/js/sql-wasm.js
+++
@@ -1,9 +0,0 @@
-// Подключение sql.js через CDN. Если хотите, можно удалить этот файл и подключить библиотеку напрямую.
-if (typeof initSqlJs === "undefined") {
-  var script = document.createElement("script");
-  script.src = "https://cdnjs.cloudflare.com/ajax/libs/sql.js/1.8.0/sql-wasm.js";
-  script.onload = () => console.log("sql.js загружен через CDN");
-  document.head.appendChild(script);
-} else {
-  console.log("sql.js уже подключён");
-}

5a468158ce2d56a3742b5917a2e0fe2b4d956117
Vsezlotov <vsezlodov@gmail.com>
2025-02-06 11:07:01 +0300

    Update app.js

--- Testerer/js/app.js
+++ Testerer/js/app.js
@@ -3,6 +3,7 @@ import { CameraManager } from './cameraManager.js';
 import { ProfileManager } from './profileManager.js';
 import { DatabaseManager } from './databaseManager.js';
 import { EventManager } from './eventManager.js';
+import { EventManager } from './questManager.js';
 
 export class App {
   constructor() {

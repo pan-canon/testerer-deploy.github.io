e8de14153c0e1be0674bcdcd181d2835f11499c8
Vsezlotov <vsezlodov@gmail.com>
2025-02-06 14:05:08 +0300

    uptd

--- Testerer/js/app.js
+++ Testerer/js/app.js
@@ -438,55 +438,73 @@ async compareCurrentFrame() {
 
 
 
-// Методы для работы с изображениями (grayscale и сравнение)
-// Добавьте этот блок в конец файла app.js, перед окончанием файла
+// ДОБАВЛЯЕМ ПОСЛЕ captureSelfie()
 
-convertToGrayscale = function(canvas) {
-  const ctx = canvas.getContext("2d");
-  const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
-  const pixels = imageData.data;
-  for (let i = 0; i < pixels.length; i += 4) {
-    let avg = (pixels[i] + pixels[i + 1] + pixels[i + 2]) / 3;
-    pixels[i] = avg;
-    pixels[i + 1] = avg;
-    pixels[i + 2] = avg;
-  }
-  ctx.putImageData(imageData, 0, 0);
-  return canvas.toDataURL("image/png");
-};
+/**
+ * Конвертация canvas в градации серого, возвращает dataURL
+ */
+convertToGrayscale(canvas) {
+    const ctx = canvas.getContext("2d");
+    const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+    const pixels = imageData.data;
 
-pixelWiseComparison = function(img1, img2) {
-  let image1 = atob(img1.split(',')[1]);
-  let image2 = atob(img2.split(',')[1]);
-  let matchCount = 0;
-  const length = Math.min(image1.length, image2.length);
-  for (let i = 0; i < length; i++) {
-    if (Math.abs(image1.charCodeAt(i) - image2.charCodeAt(i)) < 100) {
-      matchCount++;
+    // Проходим по каждому пикселю (RGBA), усредняем
+    for (let i = 0; i < pixels.length; i += 4) {
+        let avg = (pixels[i] + pixels[i + 1] + pixels[i + 2]) / 3;
+        pixels[i] = avg;
+        pixels[i + 1] = avg;
+        pixels[i + 2] = avg;
     }
-  }
-  return matchCount / length;
-};
+    ctx.putImageData(imageData, 0, 0);
 
-createHistogram = function(img) {
-  let hist = new Array(256).fill(0);
-  let imgData = atob(img.split(',')[1]);
-  for (let i = 0; i < imgData.length; i++) {
-    hist[imgData.charCodeAt(i)]++;
-  }
-  return hist;
-};
+    return canvas.toDataURL("image/png");
+}
 
-histogramComparison = function(img1, img2) {
-  let hist1 = this.createHistogram(img1);
-  let hist2 = this.createHistogram(img2);
-  let diff = 0;
-  for (let i = 0; i < hist1.length; i++) {
-    diff += Math.abs(hist1[i] - hist2[i]);
-  }
-  const total1 = hist1.reduce((a, b) => a + b, 0);
-  return 1 - (diff / (total1 * 1.2));
-};
+/**
+ * Пиксельная корреляция (сравниваем бинарные байты двух base64-картинок)
+ */
+pixelWiseComparison(img1, img2) {
+    let image1 = atob(img1.split(',')[1]);
+    let image2 = atob(img2.split(',')[1]);
 
+    let matchCount = 0;
+    // Перебираем все байты
+    for (let i = 0; i < image1.length && i < image2.length; i++) {
+        if (Math.abs(image1.charCodeAt(i) - image2.charCodeAt(i)) < 100) {
+            matchCount++;
+        }
+    }
+    return matchCount / Math.min(image1.length, image2.length);
+}
+
+/**
+ * Гистограммная корреляция (сравниваем распределение яркостей)
+ */
+histogramComparison(img1, img2) {
+    let hist1 = this.createHistogram(img1);
+    let hist2 = this.createHistogram(img2);
+
+    let diff = 0;
+    for (let i = 0; i < hist1.length; i++) {
+        diff += Math.abs(hist1[i] - hist2[i]);
+    }
+
+    // Числитель: сумма отклонений, знаменатель: суммарное число пикселей * некий коэффициент
+    let totalPixels1 = hist1.reduce((a, b) => a + b, 0);
+    return 1 - (diff / (totalPixels1 * 1.2));
+}
+
+/**
+ * Создаём гистограмму (256 уровней) из base64
+ */
+createHistogram(img) {
+    let hist = new Array(256).fill(0);
+    let imgData = atob(img.split(',')[1]);
+
+    for (let i = 0; i < imgData.length; i++) {
+        hist[imgData.charCodeAt(i)]++;
+    }
+    return hist;
+}
 
 }

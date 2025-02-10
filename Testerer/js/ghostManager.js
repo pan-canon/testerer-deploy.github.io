219869a6ad4188e59240bd9176362b9f576e4462
Vsezlotov <vsezlodov@gmail.com>
2025-02-10 23:00:40 +0300

    гзев

--- Testerer/js/ghostManager.js
+++ Testerer/js/ghostManager.js
@@ -3,10 +3,10 @@ export class GhostManager {
     this.eventManager = eventManager;
     this.profileManager = profileManager;
     this.app = app;
-this.ghosts = [
-  { id: 1, name: "призрак 1", usedLetters: [] },
-  { id: 2, name: "призрак 2", usedLetters: [] }
-];
+    this.ghosts = [
+      { id: 1, name: "призрак 1", usedLetters: [] },
+      { id: 2, name: "призрак 2", usedLetters: [] }
+    ];
 
     this.currentGhostId = 1;
     this.currentPhenomenonIndex = 0;
@@ -25,54 +25,62 @@ this.ghosts = [
   }
 
   finishCurrentGhost() {
-    if (this.getCurrentGhost()) {
-      this.getCurrentGhost().isFinished = true;
-      console.log(`Призрак ${this.getCurrentGhost().name} завершен.`);
+    const ghost = this.getCurrentGhost();
+    if (ghost) {
+      ghost.isFinished = true;
+      console.log(`Призрак ${ghost.name} завершен.`);
       this.saveState();
     }
   }
 
   isCurrentGhostFinished() {
-    return this.getCurrentGhost() && this.getCurrentGhost().isFinished;
+    const ghost = this.getCurrentGhost();
+    return ghost && ghost.isFinished;
   }
 
+  checkFinalQuestActivation() {
+    const ghost = this.getCurrentGhost();
+    if (!ghost) return;
+    
+    const nameLength = ghost.name.length;
+    const usedLetters = ghost.usedLetters.length;
 
-checkFinalQuestActivation() {
-  const ghost = this.getCurrentGhost();
-  const nameLength = ghost.name.length;
-  const usedLetters = ghost.usedLetters.length;
-
-  // Если использованы все буквы, активируем финальный квест
-  if (usedLetters >= nameLength - 2) { // Т.е. когда все, кроме последних 2 букв
-    this.activateFinalQuest();
-  }
-}
-
-activateFinalQuest() {
-  console.log("Финальный квест активирован для Призрака 1");
-  this.app.questManager.activateQuest("final_quest"); // Активируем финальный квест
-  this.finishCurrentGhost();
-}
-
-
-async triggerNextPhenomenon() {
-  const ghost = this.getCurrentGhost();
-  if (!ghost) return;
-  
-  // Определяем следующую букву из имени призрака, которая будет использована
-  const nextLetter = ghost.name.charAt(ghost.usedLetters.length);  // Берем следующую букву
-  
-  // Если буква ещё не использована, активируем её
-  if (!ghost.usedLetters.includes(nextLetter)) {
-    this.markLetterAsUsed(nextLetter);
-    await this.eventManager.addGhostDiaryEntry(`Использована буква: ${nextLetter}`);
-    console.log(`Добавлена буква в дневник: ${nextLetter}`);
+    // Если использованы все буквы, активируем финальный квест
+    if (usedLetters >= nameLength - 2) { // Т.е. когда все, кроме последних 2 букв
+      this.activateFinalQuest();
+    }
   }
 
-  // Проверка на финальный квест
-  this.checkFinalQuestActivation();
-}
+  activateFinalQuest() {
+    console.log("Финальный квест активирован для Призрака 1");
+    this.app.questManager.activateQuest("final_quest"); // Активируем финальный квест
+    this.finishCurrentGhost();
+  }
 
+  async triggerNextPhenomenon() {
+    const ghost = this.getCurrentGhost();
+    if (!ghost) return;
+    
+    const nextLetter = ghost.name.charAt(ghost.usedLetters.length);  // Берем следующую букву
+    
+    // Если буква ещё не использована, активируем её
+    if (!ghost.usedLetters.includes(nextLetter)) {
+      this.markLetterAsUsed(nextLetter);
+      await this.eventManager.addGhostDiaryEntry(`Использована буква: ${nextLetter}`);
+      console.log(`Добавлена буква в дневник: ${nextLetter}`);
+    }
+
+    // Проверка на финальный квест
+    this.checkFinalQuestActivation();
+  }
+
+  markLetterAsUsed(letter) {
+    const ghost = this.getCurrentGhost();
+    if (ghost && !ghost.usedLetters.includes(letter)) {
+      ghost.usedLetters.push(letter);
+      console.log(`Буква ${letter} помечена как использованная.`);
+    }
+  }
 
   resetGhostChain() {
     this.currentGhostId = 1;

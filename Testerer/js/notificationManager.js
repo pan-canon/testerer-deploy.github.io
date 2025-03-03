e92fb94bc44eb6e13c7a01c8e1b5c1687afde8d6
Vsezlotov <vsezlodov@gmail.com>
2025-03-04 01:56:30 +0300

    Update notificationManager.js

--- Testerer/js/notificationManager.js
+++ Testerer/js/notificationManager.js
@@ -1,18 +1,17 @@
-class NotificationManager {
-    showNotification(message) {
-        if (!("Notification" in window)) {
-            console.warn("⚠️ Уведомления не поддерживаются в этом браузере.");
-            return;
-        }
-
-        if (Notification.permission === "granted") {
-            new Notification(message);
-        } else if (Notification.permission !== "denied") {
-            Notification.requestPermission().then(permission => {
-                if (permission === "granted") {
-                    new Notification(message);
-                }
-            });
-        }
+export class NotificationManager {
+  showNotification(message) {
+    if (!("Notification" in window)) {
+      console.warn("⚠️ Notifications are not supported in this browser.");
+      return;
     }
+    if (Notification.permission === "granted") {
+      new Notification(message);
+    } else if (Notification.permission !== "denied") {
+      Notification.requestPermission().then(permission => {
+        if (permission === "granted") {
+          new Notification(message);
+        }
+      });
+    }
+  }
 }

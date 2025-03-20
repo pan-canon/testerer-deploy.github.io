04dcbabf2c7c6bdab2b5e1af36225c0d680e1b88
Vsezlotov <vsezlodov@gmail.com>
2025-03-20 22:30:29 +0300

    Update ViewManager.js

--- Testerer/game/src/managers/ViewManager.js
+++ Testerer/game/src/managers/ViewManager.js
@@ -113,7 +113,9 @@ export class ViewManager {
    */
   async loadScreen(screenName, data = {}) {
     try {
-      const response = await fetch(`${window.location.origin}/src/templates/${screenName}.html`);
+const basePath = window.location.pathname.split("/").slice(0, -1).join("/");
+const response = await fetch(`${basePath}/src/templates/${screenName}.html`);
+
       if (!response.ok) {
         throw new Error(`Failed to load template: ${screenName}.html`);
       }

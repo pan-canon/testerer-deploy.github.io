7f7bd4d9fe8e331020bd25697371a2bfd566206c
Vsezlotov <vsezlodov@gmail.com>
2025-04-17 17:33:38 +0300

    aasas

--- Testerer/game/src/managers/QuestManager.js
+++ Testerer/game/src/managers/QuestManager.js
@@ -218,9 +218,9 @@ export class QuestManager {
         }
       }
     });
-// Обновляем кнопку Post после восстановления UI
-if (this.app.ghostManager && typeof this.app.ghostManager.updatePostButtonState === 'function') {
-  this.app.ghostManager.updatePostButtonState();
-}
+    // Обновляем кнопку Post после восстановления UI
+    if (this.app.ghostManager && typeof this.app.ghostManager.updatePostButtonState === 'function') {
+      this.app.ghostManager.updatePostButtonState();
+    }
   }
 }

b38f5262862483ba50889dc6b301b3d3215c967c
Vsezlotov <vsezlodov@gmail.com>
2025-05-30 13:22:13 +0300

    uptd

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,3 @@
-// babel.config.js in project root
-
-// Use the shared Babel configuration from the game-config package
-module.exports = require('@pan-canon/pd_game-config/.babelrc');
+// testerer-deploy.github.io/babel.config.js
+// Pull in the shared Babel config from the package
+module.exports = require('@pan-canon/pd_game-config/babel.config.js');

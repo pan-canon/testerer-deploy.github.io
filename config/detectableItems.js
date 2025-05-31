f13e2365aba0ebb84610d61c40487599ba9d6286
pan-canon <pan-canon@users.noreply.github.com>
2025-05-31 23:54:37 +0000

    deploy: b1bb0c9e4beef2cb05b2a57a4e86022f4395f216

---
+++ config/detectableItems.js
@@ -0,0 +1,14 @@
+// src/config/detectableItems.js
+// List of household items for repeating quest detection
+export const detectableItems = [
+  'toilet',
+  'clock',
+  'lamp',
+  'chair',
+  'table',
+  'couch',
+  'bed',
+  'refrigerator',
+  'book',
+  'cup'
+];
